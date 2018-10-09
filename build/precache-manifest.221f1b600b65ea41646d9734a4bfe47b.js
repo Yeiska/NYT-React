76ad7720a7ce6a9451218cf52575995df99b5140
Yeiska <yeiska83@yahoo.com>
2018-10-08 20:57:19 -0400

    adding build

---
+++ build/precache-manifest.221f1b600b65ea41646d9734a4bfe47b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4a61345734694d2168bb",
+    "url": "/static/js/main.4a613457.chunk.js"
+  },
+  {
+    "revision": "c4d88402410281cf8fd8",
+    "url": "/static/js/1.c4d88402.chunk.js"
+  },
+  {
+    "revision": "4a61345734694d2168bb",
+    "url": "/static/css/main.0f6492ad.chunk.css"
+  },
+  {
+    "revision": "da50e70de98fef804654f2e14b65206c",
+    "url": "/index.html"
+  }
+];
